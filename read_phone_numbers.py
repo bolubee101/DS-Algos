760ac641fcd5798fd19abf8026f5bd1d38bd0cdc
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-03-17 04:15:09 +0100

    Update read_phone_numbers.py

--- read_phone_numbers.py
+++ read_phone_numbers.py
@@ -9,6 +9,6 @@ case #1: print out the digits
 
 #number of test cases
 test_cases = int(input("How many numbers do you want to test?: "))
-x = [input() for i in range (test_cases - 1)]
-y = input().split( )
+accept_numbers = [input() for i in range (test_cases - 1)]
+different_numbers = input().split( )
 
