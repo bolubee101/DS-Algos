8ae9b0f5c4ad5c7cff97c4536d03182ead593730
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-03-17 04:13:03 +0100

    Add files via upload

---
+++ phone_numbers.py
@@ -0,0 +1,14 @@
+'''
+-no of test cases
+-input phone number according to
+dividing format 
+-output
+case #1: print out the digits 
+'''
+
+
+#number of test cases
+test_cases = int(input("How many numbers do you want to test?: "))
+x = [input() for i in range (test_cases - 1)]
+y = input().split( )
+
