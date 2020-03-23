2cf9998689bee06527b4e0076857435428a64d67
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-03-23 13:59:05 +0100

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,26 @@
+'''
+import re
+pattern = re.compile(r"6([0-9]).(-)")
+num = 99876 9-8
+x = pattern.match(num)
+print(x)
+'''
+
+import random
+import math
+def min_loss(price):
+    x = []
+    #output = list(map(operator.sub, price,y))
+    output = [price[i] - y[j] for i in range(len(price)-1) for j in range(len(y)-1)]
+    for i in output:
+         #print((output))
+        if i > 0:
+            #print(i)
+            x.append(i)
+    print(min(x))   
+house_data = int(input('How many years is the house valid for: '))
+price = list(map(int, input("price of house").rstrip().split()))
+y = price[:]
+random.shuffle(y)
+
+min_loss(price)
