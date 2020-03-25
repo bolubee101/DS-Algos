0600196904e07071ed2374e018fda4ee97a40ee7
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-03-25 23:07:19 +0100

    Add files via upload
    A script that recursively adds up individual members of a string argument of numbers until we get a super single digit.

---
+++ recursive_digit.py
@@ -0,0 +1,20 @@
+'''
+RECURSIVE DIGIT
+function name: superdigit
+n = string rep of an integer
+k = an integer representing the  number of concatenation
+p = single super digit gotten from adding individual digits of n
+'''
+def recurse(digit_sum_y):
+	while True:
+		#print(digit_sum_y)
+		if digit_sum_y < 10:
+			return digit_sum_y
+		print(digit_sum_y % 10 + recurse(digit_sum_y // 10))
+		break 
+string_num = '123'
+iteration_k = 3
+multiplier_z = string_num * iteration_k
+digit_sum_y = sum(map(int,multiplier_z))
+
+recurse(digit_sum_y)
