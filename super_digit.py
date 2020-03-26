41fdaf23e446fa585f9aaa72115cabfb55518d8c
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-03-26 21:27:10 +0100

    Update super_digit.py
    Works.....

--- super_digit.py
+++ super_digit.py
@@ -8,13 +8,22 @@ p = single super digit gotten from adding individual digits of n
 def recurse(digit_sum_y):
 	while True:
 		#print(digit_sum_y)
-		if digit_sum_y < 10:
-			return digit_sum_y
-		print(digit_sum_y % 10 + recurse(digit_sum_y // 10))
-		break 
-string_num = '123'
-iteration_k = 3
+		if digit_sum_y < 10 or digit_sum_y <= 30:
+			return (digit_sum_y)
+		a = digit_sum_y % 10 + recurse(digit_sum_y // 10)
+		print(a)
+		if a > 10:
+			print(a % 10 + recurse(a // 10))
+		else:
+			print(a)
+		break
+
+
+string_num = input('')
+iteration_k = int(input())
 multiplier_z = string_num * iteration_k
+print(multiplier_z)
 digit_sum_y = sum(map(int,multiplier_z))
+print(f' digit_sum is {digit_sum_y}')
 
 recurse(digit_sum_y)
